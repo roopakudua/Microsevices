e482ebd3623adeffda0c4265410d49b86fdb3b5b
avkumar19 <avkumar_19@hotmail.com>
2018-07-02 11:59:21 +0000

    Auto

---
+++ deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+echo docker Image loaction along with tag
+read image_add
+echo Enter conatiner name
+read container_name
+echo Enter Service Name
+read servicename
+echo Enter Deployment Name
+read deploy_name
+echo Enter Deployment Version
+read deploy_ver
+echo Enter No of Deployment replicas
+read deploy_replicas
+echo Enter Service API version
+read ser_api_ver
+echo Container port No of dockerised image
+read portno
+cat >> employee-data.yaml << EOF
+apiVersion: $ser_api_ver
+kind: Service
+metadata:
+  name: $servicename
+  labels:
+    app: $servicename
+spec:
+  ports:
+  - port: $portno
+    name: http
+  selector:
+    app: $servicename
+---
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: $deploy_name
+spec:
+  replicas: $deploy_replicas
+  template:
+    metadata:
+      labels:
+        app: $servicename
+        version: $deploy_ver
+    spec:
+      containers:
+      - name: $container_name
+        image: $image_add
+        imagePullPolicy: IfNotPresent
+        ports:
+        - containerPort: $portno
+EOF
